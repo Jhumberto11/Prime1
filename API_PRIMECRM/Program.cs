7d4a1b25ec9ceeecb00ca2024d57a48d67f0d2ba
JonathanH2004 <imosa253@gmail.com>
2026-09-19 00:08:09 -0600

    Initial commit - Prime CRM

---
+++ API_PRIMECRM/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using PrimeCRM_Api.Infraestructure.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+//Db Context
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
